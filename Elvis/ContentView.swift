11014a3aea584a3ea9f7898b038e218cee10568c
Kelvin Pena <NYvoyage@gmail.com>
2025-06-02 09:39:24 -0400

    push from macbook air

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            Image(systemName: "heart")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            }
         .padding()
     }
 }
