3bf5a10a06cc2f1554658cfa7c387310202c21ad
Kelvin Pena <NYvoyage@gmail.com>
2025-06-02 09:33:28 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Kelvin Pena on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
