a15e3aad6bb6875bb54de59c1f820ea03605c040
Kelvin <NYvoyager@gmail.com>
2025-06-02 16:23:33 -0400

    this work//?

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,24 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  message: String = ""
+    @State private var imageName: String = ""
+    
     var body: some View {
-        
         VStack {
-            Text("What's So Funny 'Bout")
+            
+            Text("What's so Funny 'Bout")
                 .font(.largeTitle)
                 .fontWeight(.black)
                 .foregroundStyle(.purple)
+               
             Spacer()
-            Image(systemName: "heart")
+                .padding()
+            // Center Image Changeable
+            Image(systemName:imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.black)
                 .frame(width: 300, height: 300)
-            
             Spacer()
             
-            }
+            // Bottom Text
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.purple)
+            Spacer()
+                .padding()
+            
+        }
         .padding()
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                
+                }
+        }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
     }
 }
 
